1e8524f4778bc9887303c818abf72c3ac1e98432
somhm-solutions <omhm@omhm.org>
2019-11-09 00:16:40 -0800

    Values needed for skel creation Validated

--- create-stu-teach-skels.sh
+++ create-stu-teach-skels.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Skel root Directorys to create
-$TEACHERS='/tmp/teachers-skel';
-$STUDENTS='/tmp/students-skel';
+TEACHERS='/tmp/teachers-skel';
+STUDENTS='/tmp/students-skel';
 
-# Test values 
-printf "Teachers: $TEACHERS\nStudents: $STUDENTS";
+
 
 # Skel Builds
-$TEACHERS_DIRS=(Lessons Homework Grades Attendance Scheduling Media Support);
-$STUDENTS_DIRS=(Lessons Homework Grades Media Support);
-printf "Teachers Dirs:$(TEACHERS_DIRS)\n Students Dirs:$(STUDENTS_DIRS)"
+TEACHERS_DIRS=(Lessons Homework Grades Attendance Scheduling Media Support);
+STUDENTS_DIRS=(Lessons Homework Grades Media Support);
+
+# Test values 
+printf "Teachers: $TEACHERS\nStudents: $STUDENTS\n\n";
+printf "Teachers Dirs:${TEACHERS_DIRS[*]}\n Students Dirs:${STUDENTS_DIRS[*]}";
 
 
 # Build skel directory in /tmp Place in /etc if youd like to kee
