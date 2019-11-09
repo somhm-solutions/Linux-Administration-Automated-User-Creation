b32c02fe0b69e8f52086ca6d6068d5d3ff461263
somhm-solutions <omhm@omhm.org>
2019-11-09 00:47:37 -0800

    Skel Generation Script

--- create-stu-teach-skels.sh
+++ create-stu-teach-skels.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Skel root Directorys to create
-TEACHERS='/tmp/teachers-skel';
-STUDENTS='/tmp/students-skel';
+TEACHERS='/tmp/teachers-skel/';
+STUDENTS='/tmp/students-skel/';
 
 
 
 # Skel Builds using bash Array Syntax
-TEACHERS_DIRS=(Lessons Homework Grades Attendance Scheduling Media Support);
-STUDENTS_DIRS=(Lessons Homework Grades Media Support);
+TEACHERS_DIRS=("$TEACHERS"Lessons "$TEACHERS"Homework "$TEACHERS"Grades "$TEACHERS"Attendance "$TEACHERS"Scheduling "$TEACHERS"Media "$TEACHERS"Support);
+STUDENTS_DIRS=("$STUDENTS"Lessons "$STUDENTS"Homework "$STUDENTS"Grades "$STUDENTS"Media "$STUDENTS"Support);
 
 # Test values 
-printf "Teachers: $TEACHERS\nStudents: $STUDENTS\n\n";
-printf "Teachers Dirs:${TEACHERS_DIRS[*]}\n Students Dirs:${STUDENTS_DIRS[*]}";
-
+#printf "Teachers: $TEACHERS\nStudents: $STUDENTS\n\n";
+#printf "Teachers Dirs:${TEACHERS_DIRS[*]}\n Students Dirs:${STUDENTS_DIRS[*]}";
 
 # Build skel directory in /tmp Place in /etc if youd like to kee
+mkdir -p ${STUDENTS_DIRS[*]};
+mkdir -p ${TEACHERS_DIRS[*]};
