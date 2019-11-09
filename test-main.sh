f080ec18d2ae46c5a10b6d6abed26f2462affa0d
somhm-solutions <omhm@omhm.org>
2019-11-09 04:23:41 -0800

    Test script proto

---
+++ test-main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Desc: Tests for completion of Code functionality
+
+
+# Groups to add users to
+ADMIN_GROUPS=(sudo adm);
+TEACH_GROUPS='staff';
+STU_GROUPS='student';
+
+# Users to add to system
+STUDENTS=(Andrew Oscar Teresa Bill);
+TEACHERS=(Ted Irene AAron);
+
+# Skel Dirs to Use
+TEACHERS_SKEL='/tmp/teachers-skel/';
+STUDENTS_SKEL='/tmp/students-skel/';
+
+# Default Shell
+DEFAULT_SHELL='/bin/bash'
+
+# Account Expiration Dates 
+STU_EXPIRATION='2020-01-01';
+TEACH_EXPIRATION='2020-02-01';
+ADMIN_EXPIRATION='2020-03-01';
+
+# Test Skel Dir Creation 
+
+# Test Group Creation
+
+# Test User Creation
+
+# Test User Directory Creation
+
